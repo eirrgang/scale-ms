fd2a7052ee3b13656fb20f008e899173841e4a69
M. Eric Irrgang <ericirrgang@gmail.com>
2020-07-21 14:47:57 +0300

    Work in progress: Dispatch abstractions
    * Track current context and dispatch hierarchy
    * Define dispatch protocol. e.g. 1. make resources, 2. apply resources to get callable / awaitable, 3. finalize task, get and convey return object

--- docs/executor.rst
+++ docs/executor.rst
@@ -4,3 +4,21 @@ Execution middleware
 
 Executable graphs or graph segments produced by client software are dispatched
 and translated for execution on managed computing resources.
+
+.. uml:: diagrams/runtime_interface_sequence.puml
+
+Simple work
+===========
+
+A single node of work may be executed immediately in the Worker Context,
+or not at all (if the local checkpoint state is already final).
+
+.. uml:: diagrams/runtime_immediate_sequence.puml
+
+Deferred execution
+==================
+
+Multiple graph nodes may be received in the same packet of work, or asynchronously.
+The executor may locally manage dependencies to optimize execution and data placement.
+
+.. uml:: diagrams/runtime_deferred_sequence.puml
---
+++ scalems/context/__init__.py
@@ -0,0 +1,84 @@
+"""Manage the SCALE-MS API Context.
+
+SCALE-MS optimizes data flow and data locality in part by attributing all
+workflow references to well-defined scopes. Stateful API facilities, workflow
+state, and scoped references are managed as Context instances.
+
+This module allows the Python interpreter to track a global stack or tree
+structure to allow for simpler syntax and clean resource deallocation.
+"""
+
+import concurrent.futures
+import contextvars
+
+
+class AbstractWorkflowContext(concurrent.futures.Executor):
+    """Abstract base class for SCALE-MS workflow Contexts.
+
+    A workflow context includes a strategy for dispatching a workflow
+    for execution. Instances provide the concurrent.futures.Executor
+    interface with support and semantics that depend on the Executor
+    implementation and execution environment.
+
+    Notably, we rely on the Python contextmanager protocol to regulate
+    the acquisition and release of resources, so SCALE-MS workflow
+    contexts do not initialize Executors at creation. Instead,
+    client code should use `with` blocks for scoped initialization and
+    *shutdown* of Executor roles.
+
+    TODO: Enforce centralization of Context instantiation for the interpreter process.
+    For instance:
+    * Implement a root context singleton and require acquisition of new Context
+      handles through methods in this module.
+    * Use abstract base class machinery to register Context implementations.
+    * Require Context instances to track their parent Context, or otherwise
+      participate in a single tree structure.
+    * Prevent instantiation of Command references without a reference to a Context instance.
+    """
+
+
+class DefaultContext(AbstractWorkflowContext):
+    """Manage workflow data and metadata, but defer execution to sub-contexts.
+
+    Not yet implemented or used.
+    """
+
+
+# Root workflow context for the interpreter process.
+_interpreter_context = DefaultContext()
+# Note: asyncio.create_task() automatically duplicates a nested contextvars.Context
+# for the new task.
+parent = contextvars.ContextVar('parent', default=None)
+current = contextvars.ContextVar('current', default=_interpreter_context)
+
+
+def get_context():
+    return current.get()
+
+
+def run(coroutine, **kwargs):
+    """Execute the provided coroutine object.
+
+    Abstraction for :py:func:`asyncio.run()`
+
+    Note: If we want to go this route, we should integrate with the
+    asyncio event loop policy, or obtain an event loop instance and
+    use it w.r.t. run_in_executor and set_task_factory.
+
+    .. todo:: Coordinate with RP plans for event loop contexts and concurrency module executors.
+
+    See also https://docs.python.org/3/library/asyncio-dev.html#debug-mode
+    """
+    # No automatic dispatching yet. Coroutine must be executable
+    # in the current context.
+    import asyncio
+    from asyncio.coroutines import iscoroutine
+
+    # TODO: Check whether coroutine is already executing and where.
+    if iscoroutine(coroutine):
+        return asyncio.run(coroutine, **kwargs)
+
+    # TODO: Consider generalized coroutines to be dispatched through
+    #     custom event loops or executors.
+
+    raise ValueError('Unrecognized awaitable: {}'.format(coroutine))
