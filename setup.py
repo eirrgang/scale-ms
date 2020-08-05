14b7f6803d4f65f4da31dd349e5b6273c5556023
M. Eric Irrgang <ericirrgang@gmail.com>
2020-08-05 14:14:13 +0300

    [wip] type hinting
    TODO:
    * Python Workflow support types
    * Serializer
    * Deserializer
    * Begin abstract types / Python helpers for implementations.

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='scalems',
     version='0',
     packages=['scalems'],
+    # # Uncomment to enable PEP-561 style type hinting and .pyi type hinting files.
+    # package_data={
+    #     # Conform to PEP-561
+    #     'scalems': ['py.typed']
+    # },
     url='https://github.com/SCALE-MS/scale-ms/',
     license='',
     author='SCALE-MS team',
